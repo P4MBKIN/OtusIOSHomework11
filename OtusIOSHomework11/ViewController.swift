321b26368fc574e6e0c7e9835af7ae03a427833f
Pavel Antonov <paxant@mail.ru>
2020-04-04 09:48:57 +0300

    Initial Commit

---
+++ OtusIOSHomework11/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OtusIOSHomework11
+//
+//  Created by Pavel on 04.04.2020.
+//  Copyright © 2020 OtusCourse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
